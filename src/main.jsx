401689d6a28691f75aa6222f4cb5e66865f66eb5
sanjayguwaju <sanjayguwaju@gmail.com>
2023-06-21 15:19:28 +0545

    react-router-tutorial

---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter,RouterProvider} from "react-router-dom";
+import "./index.css";
+import "./contact";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
+import Root, { loader as rootLoader , action as rootAction} from "./routes/root";
+import EditContact, {loader as contactLoader} from "./routes/edit";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
